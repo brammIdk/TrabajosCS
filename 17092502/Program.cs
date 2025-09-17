ccd62a05f3c328f91653741ad209d9a45693af1e
Pat <n0048223364upn.pe>
2025-09-17 17:04:16 -0500

    añadido de ejercicio 03

--- 17092502/Program.cs
+++ 17092502/Program.cs
@@ -10,6 +10,39 @@ namespace _17092502
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ejercicio02");
+            Console.WriteLine("Monto Pension: ");
+            double montoPension = double.Parse(Console.ReadLine());
+            Console.WriteLine("Promedio: ");
+            double promedio = double.Parse(Console.ReadLine());
+
+            if (promedio >= 0 && promedio <= 20)
+            {
+                double descuento = 0.0;
+                if (promedio >= 19)
+                {
+                    descuento = 0.20;
+                }
+                else if (promedio >= 18)
+                {
+                    descuento = 0.10;
+                }
+                else if (promedio >= 17)
+                {
+                    descuento = 0.05;
+                }
+
+                double montoDsto = montoPension * descuento;
+                double nuevoMonto = montoPension - montoDsto;
+
+                Console.WriteLine($"Monto descuento: {montoDsto:F2}");
+                Console.WriteLine($"Monto Pension: {nuevoMonto:F2}");
+            }
+            else
+            {
+                Console.WriteLine("ingrese una nota valida.");
+            }
+                Console.ReadKey();   
         }
     }
 }
---
+++ 17092503/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17092503
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("monto consumo: ");
+            double montoConsumo = double.Parse(Console.ReadLine());
+            double descuento = 1;
+            //descuento
+            if (montoConsumo <= 100)
+            {
+                descuento = 0.10;
+            }
+            else if (montoConsumo <= 200) 
+            {
+                descuento = 0.20;
+            }
+            else if (montoConsumo > 200)
+            {
+                descuento = 0.30;
+            }
+            //
+            const double IGV = 0.18; 
+            double montoDscto = montoConsumo * descuento;
+            double subTotal = montoConsumo - montoDscto;
+            double montoImpuesto = subTotal * IGV;
+            double montoTotal = subTotal + montoImpuesto;
+
+            Console.WriteLine($"monto de descuento: {montoDscto :F2}");
+            Console.WriteLine($"subtotal: {subTotal}");
+            Console.WriteLine($"monto de impuesto: {montoImpuesto}");
+            Console.WriteLine($"monto total: {montoTotal}");
+
+            Console.ReadKey();
+        }
+    }
+}

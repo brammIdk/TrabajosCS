aaf98e81319a479876939870958d32a599815357
patso <n00482233@upn.pe>
2025-10-01 17:19:14 -0500

    mayor y menor valor

--- 01102503/Program.cs
+++ 01102503/Program.cs
@@ -10,7 +10,37 @@ namespace _01102503
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("---ingrese numeros---");
+            Console.WriteLine("presione 'q' para salir");
+            long maj = 0, min = 0, cont = 0;
+            bool salir = false;
+            while(salir != true)
+            { 
+                string input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                long inNum = long.Parse(input);
+                    if (cont != 1)
+                    {
+                        min = inNum;
+                        cont++;
+                    }
+                    if (inNum >= maj)
+                    {
+                        maj = inNum;
+                    }
+                    else if (inNum < min)
+                    {
+                        min = inNum;
+                    }
+                }
+            }
+            Console.WriteLine("numero mayor: " + maj);
+            Console.WriteLine("numero menor: " + min);
         }
     }
 }

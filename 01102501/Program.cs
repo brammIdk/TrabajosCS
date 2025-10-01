3c770d0d441a9aa2c6e2dea61b21cd428fc3b054
patso <n00482233@upn.pe>
2025-10-01 15:43:47 -0500

    ejercicio de potencia

---
+++ 01102501/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01102501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1, suma = 1;
+            while(num <= 5)
+            {
+                Console.WriteLine($"numero: {num:D}");
+                suma *= num ;
+                num++;
+
+            }
+            Console.WriteLine($"sumas: {suma}");
+            /*int result = 0;
+            int numb = int.Parse(Console.ReadLine());
+            for (int i = numb; i > 0; i--)
+            {
+                result += i;
+            }
+            Console.WriteLine(result);*/
+        }
+    }
+}
---
+++ 01102502/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01102502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

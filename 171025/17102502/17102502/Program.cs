58b86f855cb4554290b8c945ee257630cf612b97
Abraham <n00482233@upn.pe>
2025-10-17 15:55:12 -0500

    17/10

---
+++ 171025/17102502/17102502/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calc;
+
+namespace _17102502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, opc, res;
+            bool exit = false;
+
+            while (!exit)
+            { 
+                MostrarTitulo();
+                Console.WriteLine("ingrese un primer valor: ");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("ingrese un segundo valor: ");
+                b = double.Parse(Console.ReadLine());
+                MostrarOp();
+                opc = double.Parse(Console.ReadLine());
+                switch (opc)
+                {
+                    case 1:
+                        Class1.OutSuma(a, b, res);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+
+
+            }
+        }
+        static void MostrarTitulo()
+        {
+            Console.WriteLine("   _____      _            _           _                 ");
+            Console.WriteLine("  / ____|    | |          | |         | |                ");
+            Console.WriteLine(" | |     __ _| | ___ _   _| | __ _  __| | ___  _ __ __ _ ");
+            Console.WriteLine(" | |    / _` | |/ __| | | | |/ _` |/ _` |/ _ \\| '__/ _` |");
+            Console.WriteLine(" | |___| (_| | | (__| |_| | | (_| | (_| | (_) | | | (_| |");
+            Console.WriteLine("  \\_____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__,_|\\___/|_|  \\__,_|");
+        }
+        static void MostrarOp()
+        {
+            Console.WriteLine("Menu de opciones: ");
+            Console.WriteLine("0. Salir");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("5. Salir");
+        }
+        
+
+    }
+}
---
+++ 171025/Calc/Calc/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Class1
+    {
+        public static void OutSuma(int a, int b, out double res)
+        {
+            res = a + b;
+        }
+        public static void OutResta(int a, int b, out double res)
+        {
+            res = a - b;
+        }
+        public static void OutMultiplicacion(int a, int b, out double res)
+        {
+            res = a * b;
+        }
+        public static void OutDivision(int a, int b, out double res)
+        {
+            if (a == 0 || b == 0)
+            {
+                Console.WriteLine("Error. Division entre 0.");
+                res = 0;
+            }
+
+            res = a / b;
+        }
+
+    }
+
+}

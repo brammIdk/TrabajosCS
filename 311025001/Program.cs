a4bc0721853a0bb7c8f497644f45eaefa61f0143
Abraham <n00482233@upn.pe>
2025-10-31 15:56:05 -0500

    Add files via upload

---
+++ 311025001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _311025002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombre = new String[3];
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine($"ingrese el valor para el indice {i}: ");
+                nombre[i] = Console.ReadLine();
+            }
+            Console.WriteLine($"Mostrando los valores almacenados");
+            foreach (string n in nombre)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ 31102501/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31102501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numEstudiantes ;
+            double sumaCalif = 0, promedio, Cmax = 0.0, Cmin = 20.0;
+
+            Console.WriteLine("ingrese la cantidad de alumnos: ");
+            numEstudiantes = int.Parse(Console.ReadLine());
+
+            double[] calificaciones = new double[numEstudiantes];
+
+            for (int i = 0; i < numEstudiantes; i++)
+            {
+                Console.WriteLine($"ingrese la nota del estudiante {i+1}");
+                calificaciones[i] = double.Parse(Console.ReadLine());
+
+                sumaCalif += calificaciones[i];
+            }
+            for (int i = 0; i < numEstudiantes; i++)
+            {
+                if (calificaciones[i] < Cmin)
+                {
+                    Cmin = calificaciones[i];
+                }
+                if (calificaciones[i] > Cmax)
+                {
+                    Cmax = calificaciones[i];
+                }
+            }
+            
+            promedio = sumaCalif / numEstudiantes;
+            Console.WriteLine($"promedio: {promedio:F2}");
+            Console.WriteLine($"calificacion minima: {Cmin}");
+            Console.WriteLine($"calificacion maxima: {Cmax}");
+
+        }
+    }
+}
+
